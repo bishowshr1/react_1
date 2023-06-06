43656677bfb7a114ace92304f8232f71e8097f20
bishowshr1 <shrestharahul223@gmail.com>
2023-06-06 10:24:08 +0545

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,17 @@ const Header = () => {
 
 
   return (
-    <header className='bg-black text-white flex justify-between px-7 py-3'>
+    <header className='bg-blue-900 text-white flex justify-between px-7 py-3 sticky top-0'>
 
       <h1>Sample Web</h1>
 
       <nav className='space-x-7'>
 
+        <NavLink to='/home'>Home</NavLink>
         <NavLink to='/about'>About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
+        <NavLink to='/contact'> Contact</NavLink>
+        <NavLink to='/view'> View</NavLink>
+
 
 
       </nav>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,51 +1,88 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router';
+import React from 'react'
 
 const HomePage = () => {
-  const [data, setData] = useState([]);
-  const [isLoad, setLoad] = useState(false);
-  const [err, setErr] = useState('');
-  const [call, setCall] = useState(false);
+  // const amount = 900;
+  // const personName = 'hello world';
 
-  const nav = useNavigate();
+  // const a = 90;
+  // const b = 100;
 
-  const getData = async () => {
-    try {
-      setLoad(true);
-      const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
-      setLoad(false);
-      setData(response.data['categories']);
-    } catch (err) {
-      setLoad(false);
-      setErr(err);
-    }
+  // // console.log(a < b);
+  // // console.log(a > b);
+  // // console.log(a <= b);
+  // // console.log(a >= b);
+  // // console.log(a === b);
+  // // console.log(a !== b);
 
-  }
-  useEffect(() => {
-    getData();
-  }, [call]);
-
-  if (isLoad) {
-    return <h1>Loading....</h1>
-  }
-
-
+  // console.log(a > b || a < b || a === b);
+  // console.log(a > b && a < b && a === b);
 
   return (
-    <div className='grid grid-cols-3 items-start p-9 gap-7 '>
-      {data.map((meal) => {
-        return <div onClick={() => {
-          nav(`/detail_page/${meal.strCategory}`, { state: meal });
-        }} key={meal.idCategory} className='space-y-3 shadow-2xl p-5 cursor-pointer'>
-          <h1 className='text-center text-2xl font-bold'>{meal.strCategory}</h1>
-          <img className='w-full' src={meal.strCategoryThumb} alt="" />
-          <p>{meal.strCategoryDescription.substring(0, 100) + '....'}</p>
-        </div>
-      })}
+    <div>
+      <div className='bg-black'>
+        {/* <h1 className='text-[20px]  text-purple-700 font-bold underline decoration-wavy tracking-wide bg-slate-300'>{`${amount} ${personName.toLocaleUpperCase()}`}</h1> */}
 
+
+        <div className="info grid justify-center items-center ml-[400px]">
+          <img className='h-300% w-500% rounded-sm' src="https://images.squarespace-cdn.com/content/v1/5a78ab8490badee028bef0e9/1568935524292-TPSLMXHD9HE6PKN02YOG/Interstellar.jpg?format=1500w" alt="" />
+
+
+          <div className='shadow-2xl py-2 px-2 leading-7 col-span-2 bg-gray-900 text-white  w-[739px] h-auto overflow-auto'>
+
+            <h1 className='font-extrabold text-white px-9'>Interstellar</h1>
+            <p>Pg-13/2h 49min/ Adventure, Drama,Sci-fi,</p>
+            <br />
+            <div className='flex justify-between px-4 py-4'>
+              <h1 className=''>SUMMARY</h1>
+
+
+              <p className='text-right  text-red-900 space-x-2'>
+
+                <i class="fa-regular fa-face-smile px-3 "></i >124
+                <i class="fa-regular fa-face-smile px-3 "></i>4</p>
+            </div>
+
+            <p className=' py-4 px-4'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias cum obcaecati quidem atque, cupiditate minima ipsa sed quibusdam. Debitis ullam, possimus accusamus non similique maiores commodi magnam laboriosam id ab.</p>
+          </div>
+
+          <div className='shadow-2xl py-4 px-4 leading-1 col-span-2 bg-gray-900 text-white text-center w-[739px] h-auto '>
+            <p className=''>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, aperiam neque! Quidem quis sed molestias quisquam non ducimus in, dolorum neque recusandae ab accusamus ut! Officiis porro, explicabo alias omnis possimus quia? Soluta natus numquam porro vitae exercitationem ipsam iste atque tempora nostrum, ducimus commodi consectetur quasi nemo expedita
+              molestiae?</p>
+            <div className='flex justify-between '>
+
+              <button className='bg-black text-red-900 border-4 border-red-800 px-4'> Watch Trailer</button>
+              <h1 className=' space-x-5'>
+
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-twitter"></i>
+
+
+
+              </h1>
+
+            </div>
+
+
+
+          </div>
+
+
+        </div>
+
+      </div>
     </div>
+
   )
 }
+
+
+
+
+
+
+
+
+
 
 export default HomePage
